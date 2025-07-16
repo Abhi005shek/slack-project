fd5409d04a0c4a21fe8f59a9b484f672341dbf87
Abhi005shek <abhishek005vk+vk.com>
2025-07-16 21:34:05 +0530

    new

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -221,6 +221,7 @@ export const Message = ({
           {isEditing ? (
             <div className="h-full w-full">
               <Editor
+              placeholder="Message..."
                 onSubmit={handleUpdate}
                 disabled={isPending}
                 defaultValue={JSON.parse(body)}
--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -43,7 +43,7 @@ export const useCreateWorkspace = () => {
         const response = await mutation(values);
         options?.onSuccess?.(response);
         return response;
-      } catch (error: Error) {
+      } catch (error: any) {
         setStatus("error");
         options?.onError?.(error);
         if (options?.throwError) {
